094876a778f2468553198a18cc7f09ce1dd843cf
Yehia Tarek <yehia@startfi.io>
2023-10-04 22:39:40 +0300

    chor: linting

--- src/interfaces/IOracl.sol
+++ src/interfaces/IOracl.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: Apache 2.0
 pragma solidity ^0.8.9;
 
-interface IOracle{
-  function fulfillOracleRequest(
-    bytes32 requestId,
-    uint256 payment,
-    address callbackAddress,
-    bytes4 callbackFunctionId,
-    uint256 expiration,
-    bytes32 data
-  ) external returns (bool);
+interface IOracle {
+    function fulfillOracleRequest(
+        bytes32 requestId,
+        uint256 payment,
+        address callbackAddress,
+        bytes4 callbackFunctionId,
+        uint256 expiration,
+        bytes32 data
+    ) external returns (bool);
 
-  function isAuthorizedSender(address node) external view returns (bool);
+    function isAuthorizedSender(address node) external view returns (bool);
 
-  function withdraw(address recipient, uint256 amount) external;
+    function withdraw(address recipient, uint256 amount) external;
 
-  function withdrawable() external view returns (uint256);
+    function withdrawable() external view returns (uint256);
 }
