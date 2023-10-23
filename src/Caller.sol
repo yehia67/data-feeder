e44fcc7b51941c494ef09e06006ae37cd48a5c59
Yehia Tarek <yehiatarek67@gmail.com>
2023-10-23 23:11:22 +0300

    Add API key support (#4)
    * feat: add a caller smart contract to test the user interaction with it
    
    * enh: rm unused feature to make the oracle more simple
    
    * feat: support api keys

---
+++ src/Caller.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: Apache 2.0
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IRandOracle {
+    function requestRandomNumber() external returns (uint256);
+}
+
+contract Caller is Ownable {
+    IRandOracle private randOracle;
+
+    mapping(uint256 => bool) requests;
+    mapping(uint256 => uint256) results;
+
+    modifier onlyRandOracle() {
+        require(msg.sender == address(randOracle), "Unauthorized.");
+        _;
+    }
+
+    event OracleAddressChanged(address indexed oracleAddress);
+    event RandomNumberRequested(uint256 indexed id);
+    event RandomNumberReceived(uint256 indexed id, uint256 number);
+
+    constructor() Ownable(msg.sender) {}
+
+    function setRandOracleAddress(address newAddress) external onlyOwner {
+        randOracle = IRandOracle(newAddress);
+
+        emit OracleAddressChanged(newAddress);
+    }
+
+    function getRandomNumber() external {
+        require(randOracle != IRandOracle(address(0)), "Oracle not initialized.");
+
+        uint256 id = randOracle.requestRandomNumber();
+        requests[id] = true;
+
+        emit RandomNumberRequested(id);
+    }
+
+    function fulfillRandomNumberRequest(uint256 randomNumber, uint256 id) external onlyRandOracle {
+        require(requests[id], "Request is invalid or already fulfilled.");
+
+        results[id] = randomNumber;
+        delete requests[id];
+
+        emit RandomNumberReceived(id, randomNumber);
+    }
+}
--- src/RandOracle.sol
+++ src/RandOracle.sol
@@ -1,16 +1,18 @@
 // SPDX-License-Identifier: Apache 2.0
 pragma solidity ^0.8.19;
 
-import "openzeppelin-contracts/contracts/access/AccessControl.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract RandOracle is AccessControl {
-    bytes32 public constant PROVIDER_ROLE = keccak256("PROVIDER_ROLE");
-    uint256 private numProviders = 0;
-    uint256 private providersThreshold = 1;
+interface ICaller {
+    function fulfillRandomNumberRequest(uint256 randomNumber, uint256 id) external;
+}
 
+contract RandOracle is Ownable {
     uint256 private randNonce = 0;
 
     mapping(uint256 => bool) private pendingRequests;
+    mapping(string => uint256) public apiKeyUsage;
+    mapping(address => string) public apiKeyByAddress;
 
     struct Response {
         address providerAddress;
@@ -20,83 +22,46 @@ contract RandOracle is AccessControl {
 
     mapping(uint256 => Response[]) private idToResponses;
 
-    constructor() {
-        grantRole(DEFAULT_ADMIN_ROLE, msg.sender); // make the deployer admin
+    modifier onlyRegistratedUser() {
+        require(bytes(apiKeyByAddress[msg.sender]).length > 0, "Unauthorized.");
+        _;
     }
 
-    function requestRandomNumber() external returns (uint256) {
-        require(numProviders > 0, " No data providers not yet added.");
+    event RandomNumberRequested(uint256 indexed id, address callerAddress);
+    event RandomNumberReturned(uint256 indexed id, uint256 randomNumber, address callerAddress);
+    event AddApiKey(string indexed _apiKey, address indexed _newUser);
 
-        randNonce++;
-        uint256 id = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % 1000;
+    constructor() Ownable(msg.sender) {}
+
+    function addNewUser(string memory _apiKey, address _newUser) external onlyOwner {
+        apiKeyByAddress[_newUser] = _apiKey;
+        emit AddApiKey(_apiKey, _newUser);
+    }
+
+    function requestRandomNumber() external onlyRegistratedUser returns (uint256) {
+        uint256 id = randNonce;
         pendingRequests[id] = true;
 
-        emit RandomNumberRequested(msg.sender, id);
+        randNonce++;
+        apiKeyUsage[apiKeyByAddress[msg.sender]]++;
+        emit RandomNumberRequested(id, msg.sender);
         return id;
     }
 
-    function returnRandomNumber(uint256 randomNumber, address callerAddress, uint256 id)
-        external
-        onlyRole(PROVIDER_ROLE)
-    {
+    function returnRandomNumber(uint256 randomNumber, address callerAddress, uint256 id) external onlyOwner {
         require(pendingRequests[id], "Request not found.");
 
         // Add newest response to list
         Response memory res = Response(msg.sender, callerAddress, randomNumber);
         idToResponses[id].push(res);
-        uint256 numResponses = idToResponses[id].length;
 
-        // Check if we've received enough responses
-        if (numResponses == providersThreshold) {
-            uint256 compositeRandomNumber = 0;
+        // Clean up
+        delete pendingRequests[id];
+        delete idToResponses[id];
 
-            // Loop through the array and combine responses
-            for (uint256 i = 0; i < idToResponses[id].length; i++) {
-                compositeRandomNumber = compositeRandomNumber ^ idToResponses[id][i].randomNumber; // bitwise XOR
-            }
+        // Fulfill request
+        ICaller(callerAddress).fulfillRandomNumberRequest(id, randomNumber);
 
-            // Clean up
-            delete pendingRequests[id];
-            delete idToResponses[id];
-
-            // Fulfill request
-            // ICaller(callerAddress).fulfillRandomNumberRequest(compositeRandomNumber, id);
-
-            emit RandomNumberReturned(compositeRandomNumber, callerAddress, id);
-        }
+        emit RandomNumberReturned(id, randomNumber, callerAddress);
     }
-
-    // Admin functions
-    function addProvider(address provider) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        require(!hasRole(PROVIDER_ROLE, provider), "Provider already added.");
-
-        _grantRole(PROVIDER_ROLE, provider);
-        numProviders++;
-
-        emit ProviderAdded(provider);
-    }
-
-    function removeProvider(address provider) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        require(!hasRole(PROVIDER_ROLE, provider), "Address is not a recognized provider.");
-        require(numProviders > 1, "Cannot remove the only provider.");
-
-        _revokeRole(PROVIDER_ROLE, provider);
-        numProviders--;
-
-        emit ProviderRemoved(provider);
-    }
-
-    function setProvidersThreshold(uint256 threshold) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        require(threshold > 0, "Threshold cannot be zero.");
-
-        providersThreshold = threshold;
-        emit ProvidersThresholdChanged(providersThreshold);
-    }
-
-    // Events
-    event RandomNumberRequested(address callerAddress, uint256 id);
-    event RandomNumberReturned(uint256 randomNumber, address callerAddress, uint256 id);
-    event ProviderAdded(address providerAddress);
-    event ProviderRemoved(address providerAddress);
-    event ProvidersThresholdChanged(uint256 threshold);
 }
